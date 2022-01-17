8976119e66553adbeed6d7089b109118db6d5e52
Thyara <thyaranonato@outlook.com>
2022-01-17 15:35:37 -0300

    Implementa validaçao de: name, age, token, talk, rate, watchedAt

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -26,7 +26,76 @@ const validatePassword = (req, res, next) => {
   return next();
 };
 
+// Requisito 04
+const validateToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  return next();
+};
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  return next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  return next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if ((!talk || !talk.watchedAt) || (!talk.rate && talk.rate !== 0)) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  return next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  // Utilizado RegExp sugerido pelo Gustavo Sant'Anna no canal do slack da turma 14B.
+  // Link: https://trybecourse.slack.com/archives/C023YHXAEGM/p1642220483084700?thread_ts=1642220305.084600&cid=C023YHXAEGM
+  const watchedAtRegex = /^([0-3][0-1]|[0-2]\d)\/(0[1-9]|1[0-2])\/\d{4}/;
+  const watchedAtTest = watchedAtRegex.test(watchedAt);
+  if (!watchedAtTest) {
+    return res.status(400).json({ message: 'O campo "watchedAt" dete ter o formato "dd/mm/aaaa"' });
+  }
+  return next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  return next();
+};
+
 module.exports = {
   validateEmail,
   validatePassword,
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
 };
